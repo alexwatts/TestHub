e8697dc5a448c511bf066cf56ab033e1b4dcc1cf
alex <alex@alwa.com>
2022-01-30 17:08:20 +0000

    Centering text, filling numerals

--- src/main/ui/src/features/results/components/HeaderColumn.tsx
+++ src/main/ui/src/features/results/components/HeaderColumn.tsx
@@ -9,7 +9,7 @@ export function HeaderColumn(props: ColumnProps) {
     function formatDate(display: string) {
         const date = new Date(display);
         return date.getDate() + '/' + date.getMonth() + 1 + '/' + date.getFullYear() + ' '
-            + date.getHours() + ':' + date.getMinutes();
+            + date.getHours()+ ':' + (date.getMinutes() < 10 ? "0" + date.getMinutes(): date.getMinutes());
     }
 
     return (
