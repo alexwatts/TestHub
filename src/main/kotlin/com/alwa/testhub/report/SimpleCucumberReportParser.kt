1e37a752bb8d8422186020754a6e27d92f9e116b
alex <alex@alwa.com>
2022-02-04 13:04:08 +0000

    Removed warnings

--- src/main/kotlin/com/alwa/testhub/report/SimpleCucumberReportParser.kt
+++ src/main/kotlin/com/alwa/testhub/report/SimpleCucumberReportParser.kt
@@ -12,12 +12,12 @@ import org.springframework.stereotype.Service
 @ConditionalOnProperty("report.cucumber.parser", havingValue="simple")
 class SimpleCucumberReportParser: ReportParser {
 
-    fun JSON_PATH_TEST_NAMES() = "\$.[*].elements.[*].name"
+    fun jsonPathTestNames() = "\$.[*].elements.[*].name"
 
-    fun JSON_PATH_FAILED_TEST_RESULT(testName: String) =
+    fun jsonPathTestNames(testName: String) =
         "\$.[*].elements.[*].[?(@.name=='${testName}')].steps.[*].result[?(@.status=='failed')]"
 
-    fun JSON_PATH_SCREENSHOT(testName: String) =
+    fun jsonPathScreenShot(testName: String) =
         "\$.[*].elements.[*].[?(@.name=='${testName}')].after.[*].embeddings[?(@.name=='screenshot')]"
 
     override fun parseTestResults(reportData: ReportData): List<TestResult> =
@@ -32,13 +32,13 @@ class SimpleCucumberReportParser: ReportParser {
             }
 
     private fun readTestNames(reportData: ReportData) =
-        JsonPath.read<JSONArray>(reportData.report, JSON_PATH_TEST_NAMES())
+        JsonPath.read<JSONArray>(reportData.report, jsonPathTestNames())
 
     private fun testIsSuccess(reportData: ReportData, testName: String) =
-        JsonPath.read<JSONArray>(reportData.report, JSON_PATH_FAILED_TEST_RESULT(testName)).isEmpty()
+        JsonPath.read<JSONArray>(reportData.report, jsonPathTestNames(testName)).isEmpty()
 
     private fun testScreenShot(reportData: ReportData, testName: String) =
-        JsonPath.read<JSONArray>(reportData.report, JSON_PATH_SCREENSHOT(testName))
+        JsonPath.read<JSONArray>(reportData.report, jsonPathScreenShot(testName))
             .map { (it as LinkedHashMap<*, *>) }
             .map { ScreenShot(it["mime_type"] as String, it["data"] as String) }
             .firstOrNull()
